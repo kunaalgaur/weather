c4d1279ad16c0a60ae228ae33bc38e572567aaac
kunaalgaur <kunaalgaurr@gmail.com>
2023-10-03 19:58:15 +0530

    New type

--- constants/types.ts
+++ constants/types.ts
@@ -40,3 +40,42 @@ export interface WeatherData {
     };
 }
 
+export interface ForecastData {
+    time_epoch: number;
+    time: string;
+    temp_c: number;
+    temp_f: number;
+    is_day: number;
+    condition: {
+        text: string;
+        icon: string;
+        code: number;
+    };
+    wind_mph: number;
+    wind_kph: number;
+    wind_degree: number;
+    wind_dir: string;
+    pressure_mb: number;
+    pressure_in: number;
+    precip_mm: number;
+    precip_in: number;
+    humidity: number;
+    cloud: number;
+    feelslike_c: number;
+    feelslike_f: number;
+    windchill_c: number;
+    windchill_f: number;
+    heatindex_c: number;
+    heatindex_f: number;
+    dewpoint_c: number;
+    dewpoint_f: number;
+    will_it_rain: number;
+    chance_of_rain: number;
+    will_it_snow: number;
+    chance_of_snow: number;
+    vis_km: number;
+    vis_miles: number;
+    gust_mph: number;
+    gust_kph: number;
+    uv: number;
+}
