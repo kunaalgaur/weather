270c4a1164a201789beaa12394f4f3fe9b93184b
kunaalgaur <kunaalgaurr@gmail.com>
2023-10-03 12:05:08 +0530

    Updated styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import CurrentWeather from '@/components/card/CurrentWeather/CurrentWeather';
+import styles from './page.module.css';
 
 const page = () => {
     return (
-        <div>
-            <CurrentWeather />
+        <div id={styles.container}>
+            <div id={styles.top}>
+                <CurrentWeather />
+                <div></div>
+                <div></div>
+            </div>
+            <div id={styles.bottom}>
+                <div></div>
+                <div></div>
+            </div>
         </div>
     );
 };
