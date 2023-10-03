ecf2b575f4c2e75b1064a95720c2a925b21971d9
kunaalgaur <kunaalgaurr@gmail.com>
2023-10-03 18:09:35 +0530

    Added Loading UI

--- components/card/CurrentWeather/CurrentWeather.tsx
+++ components/card/CurrentWeather/CurrentWeather.tsx
@@ -1,49 +1,79 @@
 'use client';
 
+import { WiThermometer, WiRainMix, WiSandstorm } from 'react-icons/wi';
 import { useGetCurrentPosition } from '@/hooks/useGetCurrentPosition';
-import { useGetCurrentTime } from '@/hooks/useGetCurrentTime';
 import { useGetCurrentWeather } from '@/hooks/useGetCurrentWeather';
+import { useGetCurrentTime } from '@/hooks/useGetCurrentTime';
+import styles from './CurrentWeather.module.css';
+import Loading from '../Loading/Loading';
 import Image from 'next/image';
 
 const CurrentWeather = () => {
-    const [latitude, longitude] = useGetCurrentPosition();
+    const { latitude, longitude } = useGetCurrentPosition();
     const time = useGetCurrentTime();
     const { weather, loading } = useGetCurrentWeather(
         latitude as number,
         longitude as number
     );
 
-    console.log(latitude, longitude);
-
     if (loading) {
-        return <span>Loading</span>;
+        return <Loading />;
     }
 
     return (
-        <div>
-            <div>
-                <span>Current Weather</span>
-                <span>{time}</span>
+        <div id={styles.container}>
+            <div id={styles.top}>
+                <span id={styles.heading}>Current Weather</span>
+                <span id={styles.time}>{time}</span>
             </div>
-            <div>
-                <div>
+            <div id={styles.middle}>
+                <div id={styles.middleLeft}>
                     <Image
-                        src={
-                            ('https:' +
-                                weather?.current.condition.icon) as string
-                        }
+                        src={`/${
+                            weather?.current.condition.text as string
+                        }.png`}
                         alt=""
                         height={0}
                         width={0}
                         style={{ height: '100%', width: '100%' }}
+                        unoptimized
                     />
                 </div>
-                <div>
-                    <span>{weather?.current.temp_c}</span>
-                    <span></span>
+                <div id={styles.middleRight}>
+                    <span id={styles.temp}>
+                        {weather?.current.temp_c} &deg; C
+                    </span>
+                    <span id={styles.condition}>
+                        {weather?.current.condition.text}
+                    </span>
                 </div>
             </div>
-            <div></div>
+            <div id={styles.bottom}>
+                <div className={styles.children}>
+                    <WiThermometer className={styles.icon} />
+                    <span className={styles.childrenText}>
+                        {weather?.current.humidity}
+                    </span>
+                </div>
+                <div className={styles.children}>
+                    <WiRainMix className={styles.icon} />
+                    <span className={styles.childrenText}>
+                        {weather?.current.precip_in}%
+                    </span>
+                </div>
+                <div className={styles.children}>
+                    <span>UV</span>
+                    <span className={styles.childrenText}>
+                        {weather?.current.uv}
+                    </span>
+                </div>
+                <div className={styles.children}>
+                    <WiSandstorm className={styles.icon} />
+                    <span className={styles.childrenText}>
+                        {weather?.current.wind_kph}km/hr
+                    </span>
+                </div>
+            </div>
         </div>
     );
 };
