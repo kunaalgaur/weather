bd5740eff021c03acc761ad4208adb31cd3b4ef0
kunaalgaur <kunaalgaurr@gmail.com>
2023-10-03 19:58:39 +0530

    updated types

--- hooks/useForecast.ts
+++ hooks/useForecast.ts
@@ -1,10 +1,10 @@
-import { WeatherData } from '@/constants/types';
+import { ForecastData } from '@/constants/types';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 
 export const useForecast = (latitude: number, longitude: number) => {
-    const [weather, setWeather] = useState<any>(null);
+    const [weather, setWeather] = useState<ForecastData[] | null>(null);
     const [error, setError] = useState<string | null>(null);
     const [loading, setLoading] = useState(true);
 
@@ -19,7 +19,7 @@ export const useForecast = (latitude: number, longitude: number) => {
                         `${API}/forecast.json?key=${API_KEY}&q=${latitude},${longitude}`
                     );
                     const response = await res.data;
-                    setWeather(response);
+                    setWeather(response.forecast.forecastday.hour);
                 } catch (error: any) {
                     toast.error(error.message);
                     return setError(error.message);
